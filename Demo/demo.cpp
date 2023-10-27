7a20bca81d790d4c060f4742ab902039e63f16e6
meyok <2553774545@qq.com>
2023-10-27 22:20:18 +0800

    init(ALL): Init AlgorithmDemo Project.

---
+++ Demo/demo.cpp
@@ -0,0 +1,12 @@
+#include "demo.h"
+
+#include <iostream>
+
+void demo()
+{
+    using std::cout;
+    using std::endl;
+
+    cout << "demo" << endl;
+
+}
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "demo.h"
+
+int main()
+{
+    demo();
+}
