c10096cb5d22ef50006320d45f7e90039ee368ba
meyok <2553774545@qq.com>
2023-10-28 20:42:58 +0800

    fix(String): Modify brute-force and kmp.

--- Demo/demo.cpp
+++ Demo/demo.cpp
@@ -5,8 +5,10 @@
 #include "demo.h"
 
 
-int bruteForce(const std::string & s, const std::string & t, const int pos)
+int bruteForce(const std::string & s, const std::string & t, int pos)
 {
+    if (pos < 0) { pos = 0; }
+
     // 比较时，i 为主串的索引，j 为子串的索引。start 为每次匹配时子串在主串比较的起始位置。
     int i, j, start;
     i = start = pos;
@@ -17,31 +19,30 @@ int bruteForce(const std::string & s, const std::string & t, const int pos)
     int tLen = t.length();
 
     // 开始循环比较
-    while (i < sLen && j < tLen)
+    while (start <= sLen - tLen && j < tLen)
     {
         if (s[i] == t[j]) { ++i; ++j; }
         else { i = ++start; j = 0;}
     }
 
     // j 到达子串长度说明比较成功
-    if (j >= tLen) { return start; }
-    else { return -1; }
+    return j >= tLen ? start : -1;
 }
 
 
-void next_init(int t, int i, std::vector<int> & next, const std::string & T)
+int nextInit(const std::string & T, std::vector<int> & next, int i, int j)
 {
-    t = next[t - 1];
-    if (T[t] == T[i]) { next[i] = t + 1; }
-    else
-    {
-        if (t == 0) { next[i] = 0; }
-        else { next_init(t, i, next, T); }
-    }
+    if (i == 0) { return 0; }
+    j = next[j - 1];
+    return T[i] == T[j] ? j + 1
+        : j == 0 ? 0
+        : nextInit(T, next, i, j);
 }
 
-int kmp(const std::string & s, const std::string & t, const int pos)
+int kmp(const std::string & s, const std::string & t, int pos)
 {
+    if (pos < 0) { pos = 0; }
+
     // 比较时，i 为主串的索引，j 为子串的索引。start 为每次匹配时子串在主串比较的起始位置。
     int i, j, start;
     i = start = pos;
@@ -53,11 +54,7 @@ int kmp(const std::string & s, const std::string & t, const int pos)
 
     // 初始化 next 数组
     std::vector<int> next(tLen, 0);
-    for (int i = 1; i < tLen; i++)
-    {
-        int j = i;
-        next_init(j, i, next, t);
-    }
+    for (int m = 0; m < tLen; m++) { next[m] = nextInit(t, next, m, m); }
 
     // 比较字串，注意与 Brute-Force 不一样的是，当匹配不一致时，start、i、j 的修改。
     while (i < sLen && j < tLen)
@@ -75,8 +72,7 @@ int kmp(const std::string & s, const std::string & t, const int pos)
     }
 
     // j 到达子串长度说明比较成功
-    if (j >= tLen) { return start; }
-    else { return -1; }
+    return j >= tLen ? start : -1;
 }
 
 void demo()
@@ -88,14 +84,17 @@ void demo()
     string s = "qweabcabcqwe";
     string t1 = "abc";
     string t2 = "";
+    string t3 = "asdasd";
 
     cout << bruteForce(s, t1, 2) << endl;
     cout << bruteForce(s, t1, 10) << endl;
     cout << bruteForce(s, t2, 10) << endl;
+    cout << bruteForce(s, t3) << endl;
 
     cout << kmp(s, t1, 2) << endl;
     cout << kmp(s, t1, 10) << endl;
     cout << kmp(s, t2, 10) << endl;
+    cout << kmp(s, t3) << endl;
 
 
 }
--- include/demo.h
+++ include/demo.h
@@ -7,34 +7,38 @@
 void demo();
 
 /**
- * @brief Brute-Force 算法实现串的匹配。
+ * @author meyok
+ * @brief 串的匹配，brute-force算法。
  * @param s 主串。
  * @param t 子串。
- * @param pos 主串起始位置，缺省值为 0。
+ * @param pos 在主串匹配的起始位置，缺省值为 0。
  * @note 需要包含头文件 string。
- * @return 成功返回位置序号，否则返回 -1。
+ * @return 匹配到的主串位置，-1 为未匹配成功。
  */
-int bruteForce(const std::string & s, const std::string & t, const int pos = 0);
+int bruteForce(const std::string & s, const std::string & t, int pos = 0);
 
 
 /**
- * @brief next 数组初始化，不含 next[0]，它已被初始化为 0。
- * @param t 上次与 next 的第 i 位比较的位置，每次计算 next[i] 时，t 会被初始化为 i。
- * @param i next 数组的第 i 位。
+ * @author meyok
+ * @brief 用于初始化 next 数组。这是个递归函数，初始化第 i 项时，前面的项需要被初始化。
+ * @param T 串。
  * @param next next 数组。
+ * @param i next 数组的第 i 项。
+ * @param j 本次寻找最长前后缀的串的位数，第一次调用时被初始化为 i。
  * @note 需要包含头文件 string、vector。
- * @param T 字串。
+ * @return next 第 i 项应该被初始化的值。
  */
-void next_init(int t, int i, std::vector<int> & next, const std::string & T);
+int nextInit(const std::string & T, std::vector<int> & next, int i, int j);
 
 /**
- * @brief KMP 算法实现串的匹配。
+ * @author meyok
+ * @brief 串的匹配，kmp算法。
  * @param s 主串。
  * @param t 子串。
- * @param pos 主串起始位置，缺省值为 0。
+ * @param pos 在主串匹配的起始位置，缺省值为 0。
  * @note 需要包含头文件 string、vector。
- * @return 成功返回位置序号，否则返回 -1。
+ * @return 匹配到的主串位置，-1 为未匹配成功。
  */
-int kmp(const std::string & s, const std::string & t, const int pos = 0);
+int kmp(const std::string & s, const std::string & t, int pos = 0);
 
 #endif
